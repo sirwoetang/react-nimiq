97fc7aea6736c5558efe19aea534b724ed5455c0
waldo <waldo@stackworx.io>
2018-01-12 15:57:45 +0100

    Deploying

--- src/banner.js
+++ src/banner.js
@@ -26,22 +26,22 @@ class Banner extends Component {
       <div style={bannerStyle.container}>
         <Logo style={bannerStyle.leftIcon}/>
         <Logo style={bannerStyle.rightIcon}/>
-        <div contentEditable='true' dangerouslySetInnerHTML={{ __html: miningAddressSVG }}
-             style={bannerStyle.leftRobohash}></div>
-        <div contentEditable='true' dangerouslySetInnerHTML={{ __html: miningAddressSVG }}
-             style={bannerStyle.rightRobohash}></div>
-        <div style={bannerStyle.message}>Patron this site</div>
-        <div style={bannerStyle.message}>Client Type: {clientType}</div>
-        <div style={bannerStyle.message}>Nimiq Status: {nimiqStatus}</div>
-        <div style={bannerStyle.message}>My Wallet Address: {myWalletAddress}</div>
-        <div style={bannerStyle.message}>My Wallet Balance: {myWalletBalance}</div>
-        <div style={bannerStyle.message}>Mining Address: {miningAddress}</div>
-        <div style={bannerStyle.message}>Mining Allowed: {miningAllowed ? 'Yes' : 'No'}</div>
-        <div style={bannerStyle.message}>Miner Active: {minerActive ? 'Yes' : 'No'}</div>
-        <div style={bannerStyle.message}>Miner Hashrate: {minerHashrate} H/s</div>
-        <div style={bannerStyle.message}>Consensus: {consensus}</div>
-        <div style={bannerStyle.message}>Head: {head}</div>
-        <div style={bannerStyle.message}>Peers: {peers}</div>
+        {/*<div contentEditable='true' dangerouslySetInnerHTML={{ __html: miningAddressSVG }}*/}
+             {/*style={bannerStyle.leftRobohash}></div>*/}
+        {/*<div contentEditable='true' dangerouslySetInnerHTML={{ __html: miningAddressSVG }}*/}
+             {/*style={bannerStyle.rightRobohash}></div>*/}
+        {/*<div style={bannerStyle.message}>Patron this site</div>*/}
+        {/*<div style={bannerStyle.message}>Client Type: {clientType}</div>*/}
+        {/*<div style={bannerStyle.message}>Nimiq Status: {nimiqStatus}</div>*/}
+        {/*<div style={bannerStyle.message}>My Wallet Address: {myWalletAddress}</div>*/}
+        {/*<div style={bannerStyle.message}>My Wallet Balance: {myWalletBalance}</div>*/}
+        {/*<div style={bannerStyle.message}>Mining Address: {miningAddress}</div>*/}
+        {/*<div style={bannerStyle.message}>Mining Allowed: {miningAllowed ? 'Yes' : 'No'}</div>*/}
+        {/*<div style={bannerStyle.message}>Miner Active: {minerActive ? 'Yes' : 'No'}</div>*/}
+        {/*<div style={bannerStyle.message}>Miner Hashrate: {minerHashrate} H/s</div>*/}
+        {/*<div style={bannerStyle.message}>Consensus: {consensus}</div>*/}
+        {/*<div style={bannerStyle.message}>Head: {head}</div>*/}
+        {/*<div style={bannerStyle.message}>Peers: {peers}</div>*/}
       </div>);
   }
 }
